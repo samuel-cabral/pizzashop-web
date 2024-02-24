ec38f414f70eb9067d873308fe5fca594bf6d079
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-02-24 16:14:24 -0300

    config: eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from './components/ui/button'
 import './global.css'
 
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="flex h-screen items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
-
