a7560e17330de955908f4e2c4373a879ada18813
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-03-11 11:52:02 -0300

    feat: add helmet

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -22,7 +23,9 @@ export function SignIn() {
   async function handleSignIn(data: SignInForm) {
     console.log(data)
 
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    await new Promise((resolve) => setTimeout(resolve, 2000)) // loading
+
+    toast.success('Enviamos um link de autenticação para seu e-mail.')
   }
 
   return (
