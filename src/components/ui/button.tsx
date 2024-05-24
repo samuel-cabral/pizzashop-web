789770b3dd076d95a82d3d1b2236f5fb1f13f968
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-05-24 15:56:53 -0300

    feat: orders page

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
@@ -22,6 +22,7 @@ const buttonVariants = cva(
       size: {
         default: 'h-10 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
+        xs: 'h-8 rounded-md px-2.5',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
       },
---
+++ src/pages/app/orders/orders.tsx
@@ -0,0 +1,87 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+export function Orders() {
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+      </div>
+      <div className="space-y-2.5">
+        <form className="flex items-center gap-2">
+          <span className="text-small font-semibold">Filtros:</span>
+          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
+        </form>
+
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Identificador</TableHead>
+                <TableHead className="w-[180px]">Realizado há</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="w-[140px]">Total do pedido</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 10 }).map((_, index) => (
+                <TableRow key={index}>
+                  <TableCell>
+                    <Button variant="outline" size="xs">
+                      <Search className="h-3 w-3" />
+                      <span className="sr-only">Detalhes do pedido</span>
+                    </Button>
+                  </TableCell>
+                  <TableCell className="font-mono text-xs font-medium">
+                    q3453lkn43tk32l532nk4l52
+                  </TableCell>
+                  <TableCell className="text-muted-foreground">
+                    15 minutos
+                  </TableCell>
+                  <TableCell>
+                    <div className="flex items-center gap-2">
+                      <span className="h-2  w-2 rounded-full bg-slate-400" />
+                      <span>Pendente</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="font-medium">
+                    Mayrla Arruda Cabral
+                  </TableCell>
+                  <TableCell className="font-medium">R$ 149,90</TableCell>
+                  <TableCell>
+                    <Button variant="outline" size="xs">
+                      <ArrowRight className="mr-2 h-3 w-3" />
+                      Aprovar
+                    </Button>
+                  </TableCell>
+                  <TableCell>
+                    <Button variant="outline" size="xs">
+                      <X className="mr-2 h-3 w-3" />
+                      Cancelar
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </>
+  )
+}
