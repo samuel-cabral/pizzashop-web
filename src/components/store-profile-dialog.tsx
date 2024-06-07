9cafdd84ffcf7db43797ff6450c9460cbdeb562e
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-06-07 15:24:33 -0300

    feat: Add StoreProfileDialog component for managing store profile information

---
+++ src/components/store-profile-dialog.tsx
@@ -0,0 +1,85 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useQuery } from '@tanstack/react-query'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+
+import { Button } from './ui/button'
+import {
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from './ui/dialog'
+import { Input } from './ui/input'
+import { Label } from './ui/label'
+import { Textarea } from './ui/textarea'
+
+const storeProfileSchema = z.object({
+  name: z.string().min(1),
+  description: z.string(),
+})
+
+type StoreProfileFormValues = z.infer<typeof storeProfileSchema>
+
+export function StoreProfileDialog() {
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+  })
+
+  const { register, handleSubmit } = useForm<StoreProfileFormValues>({
+    resolver: zodResolver(storeProfileSchema),
+    values: {
+      name: managedRestaurant?.name ?? '',
+      description: managedRestaurant?.description ?? '',
+    },
+  })
+
+  async function updateManagedRestaurant(values: StoreProfileFormValues) {
+    console.log(values)
+  }
+
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Perfil da loja</DialogTitle>
+        <DialogDescription>
+          Atualize as informações da sua loja visíveis para os clientes.
+        </DialogDescription>
+      </DialogHeader>
+
+      <form onSubmit={handleSubmit(updateManagedRestaurant)}>
+        <div className="space-y-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="name">
+              Nome
+            </Label>
+            <Input {...register('name')} className="col-span-3" id="name" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="description">
+              Descrição
+            </Label>
+            <Textarea
+              {...register('description')}
+              className="col-span-3"
+              id="description"
+            />
+          </div>
+        </div>
+
+        <DialogFooter>
+          <Button variant="ghost" type="button">
+            Cancelar
+          </Button>
+          <Button type="submit" variant="success">
+            Salvar
+          </Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  )
+}
